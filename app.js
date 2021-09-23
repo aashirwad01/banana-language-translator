7c66d6f8ac9cb0ca530c6e090fb3447a18de67d4
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2021-09-23 12:10:24 +0530

    feat-added new text area for output and inputting value from one text area to another.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var userOutput=prompt("Welcome whats your name?");
-alert("Welcome "+userOutput);
+// var userOutput=prompt("Welcome whats your name?");
+// alert("Welcome "+userOutput);
 var txtArea=document.querySelector("#txt-area");
+var txtAreaOutput=document.querySelector("#txt-area-output");
 
-    txtArea.innerText=userOutput;
-
-
+var translatebtn=document.querySelector("#btn-translate");
+translatebtn.addEventListener("click",btnClicked);
+function btnClicked(){
+    var inputvalue=txtArea.value;
+    txtAreaOutput.innerText=inputvalue;
+}
 
 
 
