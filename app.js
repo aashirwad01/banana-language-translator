7d5ec33789263553efbbd6ed4a6bbe7ba2f91b16
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2021-09-23 11:52:22 +0530

    feat-alert and prompt addded

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-// var userOutput=prompt("Welcome tetetedetdede Naam kya hai tumhara?");
-console.log("Hello");
+var userOutput=prompt("Welcome whats your name?");
+alert("Welcome "+userOutput);
+var txtArea=document.querySelector("#txt-area");
+
+    txtArea.innerText=userOutput;
+
+
+
+
 
